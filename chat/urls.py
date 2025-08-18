87022bdc610a50f8a48f438b3f5539edca71b91a
Sanji Senpai <vinskyreo@gmail.com>
2025-08-19 02:15:51 +0300

    Add migration trigger route

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import chat_view, intro_view
+from .views import chat_view, intro_view, migrate_view  # 👈 Add migrate_view
 
 urlpatterns = [
     path('', intro_view, name='intro'),
     path('chat/', chat_view, name='chat'),
+    path('migrate/', migrate_view, name='migrate'),  # 👈 Add this line
 ]
