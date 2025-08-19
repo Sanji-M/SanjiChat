0aacd8f913f11ffc3bfe6ae2c487c44706ab37b8
Sanji Senpai <vinskyreo@gmail.com>
2025-08-19 15:45:47 +0300

    Fix import and update startCommand for correct project name

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-wp)8(e2v14l3gyw!jk@d)%)c89hm!65c)5-%a&x^@)7*c0*lpj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sanjablog-4.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
